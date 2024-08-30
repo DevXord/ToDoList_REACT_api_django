4f7979dd7d41baf339be4e64ba122b60b8e5e335
DevXord <xordii.xd@gmail.com>
2024-08-30 23:18:12 +0200

    delete celan_datebase.py

--- api/management/commands/clean_database.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from ...models import Task  # Importuj model do wyczyszczenia
-
-class Command(BaseCommand):
-    help = 'Czyści bazę danych'
-
-    def handle(self, *args, **kwargs):
-        Task.objects.all().delete()
-        self.stdout.write(self.style.SUCCESS('Baza danych została wyczyszczona.'))
